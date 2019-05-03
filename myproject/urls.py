6243d40f221dae338c257482968ecda52ff910c8
Suresh Melvin Sigera <sureshmelvinsigera@users.noreply.github.com>
2019-05-03 14:46:12 -0400

    urls added

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
